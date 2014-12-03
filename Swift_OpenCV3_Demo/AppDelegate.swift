ed2a5cb772c9f73b69854fc508335f5c6adf91e2
lj <lj@ljs-MacBook-Pro.local>
2014-12-03 15:59:31 +1100

    Initial Commit

---
+++ Swift_OpenCV3_Demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swift_OpenCV3_Demo
+//
+//  Created by lj on 3/12/2014.
+//  Copyright (c) 2014 flopalm.com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swift_OpenCV3_Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift_OpenCV3_Demo
+//
+//  Created by lj on 3/12/2014.
+//  Copyright (c) 2014 flopalm.com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
